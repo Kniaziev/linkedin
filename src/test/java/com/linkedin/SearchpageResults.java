14e82715dc1207641f5a6e92fd6f0e41fdd3142c
Kniaziev <knyazev.alexandr@gmail.com>
2015-12-06 12:15:46 +0200

    can't resolve problem. can't navigate to a next link from list.

--- src/test/java/com/linkedin/SearchpageResults.java
+++ src/test/java/com/linkedin/SearchpageResults.java
@@ -18,55 +18,24 @@ public class SearchpageResults extends AbstractPage {
         this.driver = driver;
     }
 
-      @FindBy(xpath = "//div[@id='results-container']//li")
+      @FindBy(xpath = "//div[@id='results-container']//li/a[contains(@href, 'www.linkedin.com/profile')]")
       private List<WebElement> searchLinks;
 
    // [contains(@href, 'www.linkedin.com/profile')]
 
-           public  HrProfilePages openHrProfilePage(){
+          public HrProfilePages openHrProfilePage() {
+              List<String> hrefs = new ArrayList<String>();
+              List<WebElement> anchors = driver.findElements(By.xpath("//div[@id='results-container']//li/a"));
+              for ( WebElement anchor : anchors ) { hrefs.add(anchor.getAttribute("href")); }
+              for ( String href : hrefs ) { driver.get(href); }
 
+              return PageFactory.initElements(driver, HrProfilePages.class);}}
 
-               List<String> hrefs = new ArrayList<String>();
-               List<WebElement> anchors = driver.findElements(By.tagName("a"));
-               for ( WebElement anchor : anchors ) { hrefs.add(anchor.getAttribute("href"));
-               } for ( String href : hrefs ) { driver.get(href); }
-
-               return PageFactory.initElements(driver, HrProfilePages.class);
-                   }
-
-
-
-    // for(int i =0 ; i<searchLinks.size();i++){
-               //    searchLinks.get(i).click();
-             //  }
-         //  for (int i = 0; i < searchLinks.size(); i++){
-                //   searchLinks.get(i).click();}
-
-   }
 
 /*
-    String ofLinks[] = new String[searchLinks.size()];
-for (int i = 0; i < searchLinks.size(); i++){
-              ofLinks[i] = allLinks.get(i).getText();
-        }
-                for (int i = 0; i < searchLinks.size(); i++){
-        if (ofLinks[i] != null){
-        driver.findElement(By.LinkText(ofLinks[i])).click();
-        }
-
-
-  /*  public HrProfilePages openHrProfilePage() {
-        java.util.List<WebElement> links = driver.findElements(By.xpath("//div[@id='results-container']//li/a[contains(@href, 'www.linkedin.com/profile')]"));
-
-        System.out.println(links.size());
-
-        for (int i = 1; i <= links.size(); i = i + 1)
-
-        {
-            System.out.println(links.get(i).getText());
-        }
-        return PageFactory.initElements(driver, HrProfilePages.class);
-    }
-}*/
-
-
+* List<String> hrefs = new ArrayList<String>();
+ * List<WebElement> anchors = driver.findElements(By.tagName("a"));
+ * for ( WebElement anchor : anchors ) { hrefs.add(anchor.getAttribute("href")); }
+ * for ( String href : hrefs ) { driver.get(href); }
+*
+* */
